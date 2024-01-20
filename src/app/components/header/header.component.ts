6b01fdb7ca8f9d2e7c3005b93ea3d6e3e8f686ea
Aristocrat4 <galadanelia1@gmail.com>
2024-01-20 19:36:42 +0400

    fix carousel error

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  onServices() {
+    window.scroll({
+      top: 780,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
 }
