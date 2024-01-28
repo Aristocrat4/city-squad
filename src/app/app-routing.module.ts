85be7951cf2a083190b04f5d254312f1d5b86c32
Aristocrat4 <galadanelia1@gmail.com>
2024-01-28 19:32:03 +0400

    add success page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { QuoteComponent } from './components/quote/quote.component';
+import { SuccessComponent } from './components/success/success.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'get-the-price', component: QuoteComponent },
+  { path: 'success', component: SuccessComponent },
 ];
 
 @NgModule({
--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -1,7 +1,14 @@
 import { Component, ElementRef } from '@angular/core';
 import { TvFormComponent } from '../tv-form/tv-form.component';
 import { RightCalculationComponent } from '../right-calculation/right-calculation.component';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { SelectComponent } from '../select/select.component';
 import { CommonModule } from '@angular/common';
 interface TV {
@@ -14,6 +21,7 @@ interface TV {
   led?: string | null | undefined;
   date?: string | null | undefined;
   comment?: string | null | undefined;
+  active?: boolean;
 }
 @Component({
   selector: 'app-quote',
@@ -30,27 +38,72 @@ interface TV {
 })
 export class QuoteComponent {
   isContactFormVisible!: false;
-  isWarningTextVisible!: false;
-  tvList: TV[] = [];
-  constructor(private el: ElementRef) {}
-  onEdit() {
-    console.log('edit');
-    const element = this.el.nativeElement.querySelector('.labels');
-    console.log(element);
+  tvFormTest!: FormGroup;
+  totalPrice: number = 0;
+
+  constructor(private el: ElementRef, private fb: FormBuilder) {
+    this.tvFormTest = this.fb.group({
+      tvs: this.fb.array([this.createEmptyTv(true)]),
+    });
+  }
+  onEdit(tv: any, i: number) {
+    const tvs = this.tvFormTest.get('tvs') as FormArray;
+    tvs.controls.forEach((control, index) => {
+      console.log(index, i, 'index');
+      if (i === index) {
+        control.get('active')?.setValue(true);
+      }
+    });
+    console.log(tv, 'edit');
+  }
+  onSave(tv: any, i: number) {
+    const tvs = this.tvFormTest.get('tvs') as FormArray;
+    tvs.controls.forEach((control, index) => {
+      console.log(index, i, 'index');
+      if (i === index) {
+        control.get('active')?.setValue(false);
+      }
+    });
+    if (this.tvFormTest.valid && i < 4) {
+      const newTv = this.createEmptyTv(true);
+      const tvs = this.tvFormTest.get('tvs') as FormArray;
+      tvs.push(newTv);
+
+      tvs.controls.forEach((control) => {
+        if (control !== newTv) {
+          control.get('active')?.setValue(false);
+        }
+      });
+    }
   }
   onAddAnother() {
-    console.log(this.tvForm.value);
-    this.tvList.push(this.tvForm.value);
+    if (this.tvFormTest.valid) {
+      const newTv = this.createEmptyTv(true);
+      const tvs = this.tvFormTest.get('tvs') as FormArray;
+      tvs.push(newTv);
+
+      tvs.controls.forEach((control) => {
+        if (control !== newTv) {
+          control.get('active')?.setValue(false);
+        }
+      });
+    }
   }
-  tvForm = new FormGroup({
-    size: new FormControl(''),
-    mountType: new FormControl(''),
-    whereToHideWires: new FormControl(''),
-    soundBar: new FormControl(''),
-    shelves: new FormControl(''),
-    fireplace: new FormControl(''),
-    led: new FormControl(''),
-    date: new FormControl(''),
-    comment: new FormControl(''),
-  });
+  get tvsList(): FormArray {
+    return this.tvFormTest.get('tvs') as FormArray;
+  }
+  createEmptyTv(active: boolean): FormGroup {
+    return this.fb.group({
+      size: ['', Validators.required],
+      mountType: ['', Validators.required],
+      whereToHideWires: ['', Validators.required],
+      soundBar: ['', Validators.required],
+      shelves: ['', Validators.required],
+      fireplace: ['', Validators.required],
+      led: ['', Validators.required],
+      date: [''],
+      comment: [''],
+      active: active,
+    });
+  }
 }
